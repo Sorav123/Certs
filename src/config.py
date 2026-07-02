bc6b664e8adf041aa648c335a72fdf46741d626e
Sorav123 <37618853+Sorav123@users.noreply.github.com>
2026-07-03 05:07:39 +0530

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+# -----------------------------
+# SOURCE REPOSITORY
+# -----------------------------
+
+SOURCE_REPO = "https://github.com/royilkom-alt/AppleJr"
+
+# -----------------------------
+# DESTINATION
+# -----------------------------
+
+DESTINATION_REPO = "Sorav123/Certs"
+
+# -----------------------------
+# NEW PASSWORD
+# -----------------------------
+
+NEW_PASSWORD = "godripyt"
+
+# -----------------------------
+# OLD PASSWORDS
+# -----------------------------
+
+PASSWORDS = [
+
+    "applejr.net",
+
+    "1",
+
+    "AppleP12.com",
+
+]
+
+# -----------------------------
+# TEMP
+# -----------------------------
+
+TEMP = Path("temp")
+OUTPUT = Path("output")
+
+TEMP.mkdir(exist_ok=True)
+OUTPUT.mkdir(exist_ok=True)
